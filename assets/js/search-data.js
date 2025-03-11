9d8f2a84b0ff787ac1066e18317dfabcbd291227
asiraudin <70633826+asiraudin@users.noreply.github.com>
2025-03-11 15:13:41 +0000

    Deploying to gh-pages from @ LoG-RWTH/LoG-RWTH.github.io@ec9373f7b5b942bff44c69813677db3d814c6038 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,26 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Courses and Thesis Guidelines",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
